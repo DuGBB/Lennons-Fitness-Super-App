31b5914ca0cf245cf1d0f1b80f4cf315be4f47cb
Donald Biggers <DuG@tests-Air.attlocal.net>
2022-03-24 10:21:55 -0500

    PLEASE LOOK AT THE COMMENTS FOR DIRECTION TO FIX THIS HARDCODING WRONGNESS

--- client/src/components/ClassSchedule/index.js
+++ client/src/components/ClassSchedule/index.js
@@ -1,17 +1,43 @@
 // import React from "react";
-// import { useQuery } from "@apollo/client";
-// import { QUERY_CLASSES } from "../../utils/queries";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_CLASSES } from "../../utils/queries";
 
-// function ClassSchedule (){
-//     const classes = useQuery(QUERY_CLASSES);
-//     console.log(classes.data);
-//     for (const [key, value] of Object.entries(classes)) {
-//         console.log(`${key}: ${value}`);
-//       };
+function ClassSchedule() {
+  //     const classes = useQuery(QUERY_CLASSES);
+  //     console.log(classes.data);
+  //     for (const [key, value] of Object.entries(classes)) {
+  //         console.log(`${key}: ${value}`);
+  //       };
+  //     return (
+  //         <div>classschedule is working</div>
+  //     )
+  const { loading, data } = useQuery(QUERY_CLASSES);
+  useEffect(() => {
+    if (data) {
+      console.log(data);
+      data.classes.forEach((clazz) => {
+        console.log(clazz.name);
+        console.log(clazz._id);
+        console.log(clazz.days);
+        console.log(clazz.time);
+        console.log(clazz.description);
+      });
+    } else if (!loading) {
+      console.log("waiting on new data load??");
+    }
+  }, [data, loading]);
+  //   for (const [key, value] of Object.entries(activities)) {
+  //     console.log(`${key}: ${value}`);
+  //   }
+  /* Form should be built dynmaically, like the ProductList form.
+  Lines 47-67.  Special attention to lines 52-61.*/
+  return (
+    <section className="container">
+      <h2>Pick a class and Sign up today!</h2>
+      <h3> dynamically build the class sign up list here! </h3>
+    </section>
+  );
+}
 
-//     return (
-//         <div>classschedule is working</div>
-//     )
-// }
-
-// export default ClassSchedule;
+export default ClassSchedule;
